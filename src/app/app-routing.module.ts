2b438d2b7854621a5ce67533a7e3dd70fedceb89
Sumit Kamble <sumit.kamble@sensiaglobal.com>
2023-10-12 15:44:48 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './components/tasks/todo-list/todo-list.component';
+import { MainComponent } from './layout/main/main.component';
+import { PortfolioComponent } from './components/tasks/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path:'todo-list',
+    component:TodoListComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: "**",
+    component: MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from "./layout/header/header.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MainComponent } from './layout/main/main.component';
+import { InfoCardsComponent } from './components/info-cards/info-cards.component';
+import { TodoListComponent } from './components/tasks/todo-list/todo-list.component';
+import { PortfolioComponent } from './components/tasks/portfolio/portfolio.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        MainComponent,
+        InfoCardsComponent,
+        TodoListComponent,
+        PortfolioComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatGridListModule
+    ]
 })
 export class AppModule { }
